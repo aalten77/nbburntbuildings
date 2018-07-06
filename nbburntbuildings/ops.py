b942de50c5899263c5294681e6d9dc8aa54c6a07
ai-linhalten <ai-linh.alten@sjsu.edu>
2018-07-06 14:04:14 -0600

    adding ops file with reprojection and km2_area

--- nbburntbuildings/ops.py
+++ nbburntbuildings/ops.py
@@ -1,7 +1,7 @@
 from functools import partial
 import pyproj
 from shapely.geometry import shape
-from shapely import ops, geometry, cascaded_union
+from shapely import ops, geometry
 
 
 def reproject(geom, from_proj='EPSG:4326', to_proj='EPSG:26942'):
@@ -10,4 +10,4 @@ def reproject(geom, from_proj='EPSG:4326', to_proj='EPSG:26942'):
 
 def km2_area(polygons):
     reprojected_polygons = [reproject(p) for p in polygons]
-    return cascaded_union(reprojected_polygons).area * (1**-6)
+    return ops.cascaded_union(reprojected_polygons).area * 1e-6
